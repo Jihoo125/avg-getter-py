961ae0b7c572538afda4e3de538045e70ec2e945
Bruh <109279349+Jihoo125@users.noreply.github.com>
2026-02-03 11:00:40 +0900

    Implement average calculation from user input
    Calculate the average of user-input numbers.

---
+++ main.py
@@ -0,0 +1,5 @@
+avg, intAdd, inputList = 0, 0, list(map(int,input("Please enter the numbers to get an average of.:").split()
+for i in inputList():
+    intAdd += i
+avg = intAdd / len(inputList)
+print(avg)
